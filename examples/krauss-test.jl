9c80c3352ac7917f927c8c7016082da41bdffdbd
Roman Dimov <romanddimov@gmail.com>
2023-02-09 21:32:05 -0800

    cleanup kraus tests

--- examples/krauss-test.jl
+++ examples/krauss-test.jl
@@ -42,30 +42,22 @@ compl = +([Ki' * Ki for Ki in eachslice(Ks, dims=3)]...)
 
 # Transform Kraus operator into tensor
 krausidx = Index(last(size(Ks)), "Kraus")
-sinds = hcat(siteinds(rho))
-unprimed = sinds[1, :]
-primed = sinds[2, :]
-Kraw = toITensor(Ks, primed, unprimed, krausidx)
+Kraw = toITensor(Ks, prime.(sites), sites, krausidx)
 
 # Build Kdag TODO: there must be a better way
 Kdaglist = [Ki' for Ki in eachslice(Ks, dims=3)]
 Kdagarr = reshape(reduce(hcat, Kdaglist), size(Ks)...)
-Kdagraw = toITensor(Kdagarr, primed, unprimed, krausidx)
+Kdagraw = toITensor(Kdagarr, prime.(sites), sites, krausidx)
 
 # Check completeness with matrices
 res = prime(Kraw) * Kdagraw * δ(krausidx, prime(krausidx))
 
 # Put Kraus tensor into canonical form
-function getcanonicalkraus(K, krausidx)
-    U, S, V = svd(K, krausidx)
-    sindex = uniqueind(S, V)
-    return δ(krausidx, sindex) * S * V
-end
 K = getcanonicalkraus(Kraw, krausidx)
 Kdag = getcanonicalkraus(Kdagraw, krausidx)
 
 # Check completeness with tensors
-complete = apply(Kdag, *([δ(ind, prime(ind)) for ind in unprimed]...), apply_dag=true)
+complete = apply(Kdag, *([δ(ind, prime(ind)) for ind in sites]...), apply_dag=true)
 array(complete)
 
 # Test channel, make sure loss it the same
