3f2d42fce3f7f3b7ed7df5e0fae9b07be4231ad5
Roman Dimov <romanddimov@gmail.com>
2023-02-10 18:02:51 -0800

    analyze truncation of random mpo

--- examples/random-mpo-truncation.jl
+++ examples/random-mpo-truncation.jl
@@ -4,45 +4,95 @@ Pkg.activate(joinpath(@__DIR__, ".."))
 
 using ITensors
 using Plots
+using Parameters
 
 include("../src/circuit.jl")
 include("../src/utilities.jl")
 include("../src/approxchannel.jl")
+include("../src/channel-analysis.jl")
 
-"""
-Tests the channel approximation function.
-"""
+@with_kw struct TruncParams
+    nsites::Int
+    bonddim::Int
+    nkraussites::Int
+    nbondstrunc::Int
+    truncatedbonddim::Int
+    nkraus::Int
+end
 
-# generate random density matrices
-nsites = 2
-bonddim = 1000
-sites = siteinds("Qubit", nsites)
-psi = randomMPS(ComplexF64, sites, linkdims=bonddim)
-rho = density_matrix(psi)
+@with_kw struct TruncResults
+    reducedrho::MPO
+    truncrho::MPO
+    K::ITensor
+    krausidx::Index
+    kraussites::Vector{Index}
+    lossratio::Float64
+    initdimstrunc::Vector{Int}
+end
 
-# Make truncated density matrix
-nbondstrunc = 1
-centersite = nsites ÷ 2
-startsite = centersite - (nbondstrunc ÷ 2)
-siterange = startsite:(startsite+nbondstrunc)
-truncatedbonddim = 1
-trho = copy(rho)
-NDTensors.truncate!(trho, maxdim=truncatedbonddim, site_range=siterange)
+function centerrange(n, size)
+    first = (n - size) ÷ 2 + 1
+    last = first + size - 1
+    return first:last
+end
 
-# Find approximate quantum channel
-nkraus = 6
-ρ = Matrix(rho)
-ρ̃ = Matrix(trho)
-Ks, optloss, initloss, iterdata, model = approxquantumchannel(ρ, ρ̃, nkraus=nkraus)
+params = TruncParams(;
+    nsites=6,
+    bonddim=100,
+    nkraussites=2,
+    nbondstrunc=1,
+    truncatedbonddim=1,
+    nkraus=4
+)
 
-@show (initloss - optloss) / initloss
+function runtruncationapprox(params::TruncParams)
+    @unpack nsites, bonddim, nkraussites, nbondstrunc, truncatedbonddim, nkraus = params
 
-# Transform Kraus operator into tensor
-krausidx = Index(last(size(Ks)), "Kraus")
-Kraw = toITensor(Ks, prime.(sites), sites, krausidx)
-K = getcanonicalkraus(Kraw, krausidx)
+    # Generate random density
+    sites = siteinds("Qubit", nsites)
+    print("")
+    psi = normalize(randomMPS(ComplexF64, sites, linkdims=bonddim))
+    rho = density_matrix(psi)
 
-# Plot norm distribution. The U should be a diagonal matrix with entries with norm 1.
+    # Take subset of sites in the middle. Can choose to trace out other sites
+    # shouldpartialtrace = false TODO: include non-partial trace case
+    krausrange = centerrange(nsites, nkraussites)
+    kraussites = sites[krausrange]
+    reducedrho = reduced_density_matrix(rho, collect(krausrange))
+
+    # Make truncated density matrix
+    truncrange = centerrange(nkraussites, nbondstrunc + 1)
+    truncrho = copy(reducedrho)
+    NDTensors.truncate!(truncrho, maxdim=truncatedbonddim, site_range=truncrange)
+
+    # Find approximate quantum channel
+    ρ = Matrix(reducedrho)
+    ρ̃ = Matrix(truncrho)
+    Ks, optloss, initloss, iterdata, model = approxquantumchannel(ρ, ρ̃, nkraus=nkraus)
+
+    lossratio = (initloss - optloss) / initloss
+    @show lossratio
+
+    # Transform Kraus operator into tensor
+    krausidx = Index(last(size(Ks)), "Kraus")
+    Kraw = toITensor(Ks, prime.(kraussites), kraussites, krausidx)
+    K = getcanonicalkraus(Kraw, krausidx)
+
+    return TruncResults(;
+        reducedrho,
+        truncrho,
+        K,
+        krausidx,
+        kraussites,
+        lossratio,
+        initdimstrunc=[]
+    )
+end
+
+res = runtruncationapprox(params)
+@unpack K, krausidx, kraussites = res
+
+# Get norm distribution. The U should be a diagonal matrix with entries with norm 1.
 U, S, V = svd(K, krausidx)
 mags = array(diag(S))
 relmags = mags / sum(mags)
@@ -54,8 +104,11 @@ p = bar(
     title="Relative magnitudes of Kraus operators",
     legend=:none,
 )
+display(p)
 
 # Plot pauli decompositions
+p = visualize_paulidecomp(K, kraussites, title="Pauli decomposition for truncation channel")
+display(p)
 
 """
 Ideas:
