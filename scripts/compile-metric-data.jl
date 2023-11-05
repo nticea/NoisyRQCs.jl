01d74cd3717c8b07e4783f7e4d1e1ce432132cde
Roman Dimov <romanddimov@gmail.com>
2023-11-04 23:36:38 -0700

    add script for compiling metrics

---
+++ scripts/compile-metric-data.jl
@@ -0,0 +1,52 @@
+using Pkg
+Pkg.activate(joinpath(@__DIR__, "../"))
+
+using CSV
+using DataFrames
+using Glob
+
+function build_state_params(statedirname)
+    # match any digits and a single decimal point
+    pattern = r"(\d+(\.\d+)?)(L|T|noise|outer|inner)"
+    matches = eachmatch(pattern, statedirname)
+    return Dict(m[3] => parse(contains(m[1], ".") ? Float64 : Int, m[1]) for m in matches)
+end
+
+function get_t(statefilename)
+    pattern = r"state_t(\d+)"
+    m = match(pattern, statefilename)
+    return parse(Int, m[1])
+end
+
+# Define the path to the data directory
+datadir = joinpath(@__DIR__, "..", "data")
+
+# Get a list of all state directories inside the data directory
+statefiles = glob(joinpath("states*", "state_t*.csv"), datadir)
+
+# Initialize an empty DataFrame to store the combined data
+combined_filename = "combined.csv"
+combined_path = joinpath(datadir, combined_filename)
+alldfs = DataFrame[]
+
+# Loop through each state directory
+for statefile in statefiles
+    # Get the state parameters from the directory name
+    statedirname = splitpath(dirname(statefile))[end]
+    stateparams = build_state_params(statedirname)
+
+    # Get the t (timestep) from the state filename
+    statefilename = basename(statefile)
+    t = get_t(statefilename)
+
+    statedata = CSV.File(statefile) |> DataFrame
+    # Add parameters and t to record
+    insertcols!(statedata, "t" => t, stateparams...)
+
+    push!(alldfs, statedata)
+end
+
+combined = vcat(alldfs...)
+
+# Write the combined data to a master CSV file
+CSV.write(combined_path, combined)
